b93a850f9547ef3f2c4b95f67e02ba35374f2a07
mashan <handikawilwatikta@gmail.com>
2022-02-15 00:13:18 +0700

    Create new Data Types

---
+++ tipeData.py
@@ -0,0 +1,30 @@
+# tipe data Boolean
+print(True)
+
+# tipe data String
+print("Belajar python itu seru...!")
+print("Python itu Mudah...")
+
+# tipe data Integer
+print(22)
+
+# tipe data Float
+print(3.14)
+
+# tipe data Hexadecimal
+print(0xFF)
+
+# tipe data Complex
+print(1j)
+
+# tipe data List
+print([1,2,3,4,5])
+print(["Belajar", "Python", "Itu", "Seru"])
+
+# tipe data Tuples
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga", "empat", "lima"))
+
+# tipe data Dictionary
+print({1:"satu", 2:"dua", 3:"tiga", 4:"empat", 5:"lima"})
+
