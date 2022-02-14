380c1d2929ea401f9937e3293881f61857b848f5
mashan <handikawilwatikta@gmail.com>
2022-02-15 00:30:52 +0700

    Create new Operator Aritmatika

---
+++ operatorAritmatika.py
@@ -0,0 +1,42 @@
+# Belajar Operator Aritmatika
+
+# Penjumlahan
+print(1 + 2)
+apel = 7
+jeruk = 9
+buah = apel + jeruk # proses penjumlahan
+print(buah) # mencetak hasil penjumlahan
+
+# Pengurangan
+hutang = 10000
+bayar = 5000
+sisaHutang = hutang - bayar # proses pengurangan
+print("Sisa hutang:", sisaHutang)
+
+# Perkalian
+panjang = 15
+lebar = 8
+luas = panjang * lebar
+print("Luas persegi panjang:", luas)
+
+# Pembagian
+kue = 15
+anak = 5
+kuePerAnak = kue / anak
+print("Setiap anak akan mendapatkan bagian kue terbanyak", kuePerAnak)
+
+# Sisa Bagi / Modulus
+bilangan1 = 14
+bilangan2 = 5
+hasil = bilangan1 % bilangan2
+print("Sisa bagi dari", bilangan1, "dan", bilangan2, "adalah:", hasil)
+
+# Bilangat Pangkat
+bilangan3 = 8
+bilangan4 = 2
+hasilPangkat = bilangan3 ** bilangan4
+print("Hasil pangkat dari", bilangan3, "pangkat", bilangan4, "adalah:", hasilPangkat)
+
+# Pembagian Bulat
+print(10 // 3)
+# 10 dibagi 3 adalah 3.3333333333333335. Karena dibulatkan maka akan menghasilkan nilai 3
