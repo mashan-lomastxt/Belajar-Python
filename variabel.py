93b55e005fc6cff1ac6c7dd876bf4940cca06845
mashan <handikawilwatikta@gmail.com>
2022-02-15 00:20:47 +0700

    Create new Variabel

---
+++ variabel.py
@@ -0,0 +1,31 @@
+# proses memasukkan data ke dalam variabel
+nama = "Mashan"
+
+# proses menampilkan data dari variabel
+print(nama)
+
+# nilai dan tipe data dalam variabel dapat diubah
+umur = 22 # nilai awal
+print(umur) # mencetak nilai umur
+type(umur) # mengecek tipe data umur
+umur = "dua puluh dua" # nilai baru setelah diubah
+print(umur)
+type(umur)
+
+namaDepan = "Riska"
+namaBelakang = "Handika"
+nama = namaDepan + " " + namaBelakang
+umur = 22
+hobi = "Membaca"
+print("Biodata: \n", nama, "\n", umur, "\n", hobi)
+
+# contoh variabel lainnya
+inivariabel = "Hallo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print("Luas persegi panjang:", luas)
