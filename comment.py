9957ee13d02ab7f41f44af800e08491cdfccb0d9
mashan <handikawilwatikta@gmail.com>
2022-02-15 00:02:53 +0700

    first commit

---
+++ comment.py
@@ -0,0 +1,25 @@
+# ini adalah komentar
+# tulisan ini tidak akan dieksekusi
+
+# komentar dengan tanda pagar hanya bisa digunakan
+# untuk 
+# satu
+# baris
+
+"""
+penulisan komentar lebih dari satu baris yaitu
+dengan menggunakan kutip dua tiga kali dan
+ditutup dengan kutip dua tiga kali juga
+"""
+
+print("Hello World!") # ini juga komentar
+
+# print("Welcome to my world!")
+
+# komentar bisa berisi spesial karakter !#@$#$#()&^*&^%$#@!
+
+# mencetak nama
+print("mashan")
+
+# mencetak angka/integer
+print(123)
